b233ce30765e580679376cc59206ee9cc1c21610
yangxiaolan <1518634581@qq.com>
2025-07-07 22:15:21 +0800

    尚庭公寓第二次提交，定位于94集 已完成：房间支付方式管理，房间租期管理

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/LeaseTermController.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/LeaseTermController.java
@@ -3,8 +3,10 @@ package com.atguigu.lease.web.admin.controller.apartment;
 
 import com.atguigu.lease.common.result.Result;
 import com.atguigu.lease.model.entity.LeaseTerm;
+import com.atguigu.lease.web.admin.service.LeaseTermService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,21 +16,38 @@ import java.util.List;
 @RestController
 public class LeaseTermController {
 
+    //先注入service
+    @Autowired
+    private LeaseTermService service;
+
+
     @GetMapping("list")
     @Operation(summary = "查询全部租期列表")
     public Result<List<LeaseTerm>> listLeaseTerm() {
-        return Result.ok();
+
+        //TODO 查询全部租期列表
+        List<LeaseTerm> list = service.list();
+
+        return Result.ok(list);
     }
 
     @PostMapping("saveOrUpdate")
     @Operation(summary = "保存或更新租期信息")
     public Result saveOrUpdate(@RequestBody LeaseTerm leaseTerm) {
+
+        //TODO 保存或更新租期信息
+        service.saveOrUpdate(leaseTerm);
+
         return Result.ok();
     }
 
     @DeleteMapping("deleteById")
     @Operation(summary = "根据ID删除租期")
     public Result deleteLeaseTermById(@RequestParam Long id) {
+
+        //TODO 根据ID删除租期
+        service.removeById(id);
+
         return Result.ok();
     }
 }
--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/PaymentTypeController.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/PaymentTypeController.java
@@ -3,8 +3,11 @@ package com.atguigu.lease.web.admin.controller.apartment;
 
 import com.atguigu.lease.common.result.Result;
 import com.atguigu.lease.model.entity.PaymentType;
+import com.atguigu.lease.web.admin.service.PaymentTypeService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,21 +18,51 @@ import java.util.List;
 @RestController
 public class PaymentTypeController {
 
+    @Autowired
+    private PaymentTypeService service;
+
     @Operation(summary = "查询全部支付方式列表")
     @GetMapping("list")
     public Result<List<PaymentType>> listPaymentType() {
-        return Result.ok();
+        //TODO 查询全部支付方式的方法实现
+        //用service的list方法直接查询
+        //
+        //存在问题：要过滤逻辑删除字段为 1 的数据
+        //
+        //方法一：给yml文件做全局配置
+        //mybatis-plus:
+        //  global-config:
+        //    db-config:
+        //      logic-delete-field: is_delete  #对应数据库中表示逻辑删除的字段
+        //      logic-delete-value: 1  #表示逻辑删除成功的值
+        //      logic-not-delete-value: 0  #表示逻辑未删除的
+        //
+        //方法二：直接给isdelete字段加@TableLogic注解
+        //
+        //两种方法二选一
+
+        //注意事项：逻辑删除功能只对Mybatis-Plus自动注入的sql起效，也就是说，对于手动在`Mapper.xml`文件配置的sql不会生效，需要单独考虑。
+        List<PaymentType> list = service.list();
+        return Result.ok(list);
     }
 
     @Operation(summary = "保存或更新支付方式")
     @PostMapping("saveOrUpdate")
     public Result saveOrUpdatePaymentType(@RequestBody PaymentType paymentType) {
+
+        //TODO 保存或更新支付方式
+        service.saveOrUpdate(paymentType);
+
         return Result.ok();
     }
 
     @Operation(summary = "根据ID删除支付方式")
     @DeleteMapping("deleteById")
     public Result deletePaymentById(@RequestParam Long id) {
+
+        //TODO 根据ID删除支付方式
+        service.removeById(id);
+
         return Result.ok();
     }
 
