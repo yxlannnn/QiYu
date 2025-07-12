0f28dd2ac36122d9bd801b02317b059c338dd2ca
yangxiaolan <1518634581@qq.com>
2025-07-12 16:21:43 +0800

    尚庭公寓第六次提交，定位于105集 已完成：有关公寓杂费相关的接口开发

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/AttrController.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/AttrController.java
@@ -7,6 +7,7 @@ import com.atguigu.lease.model.entity.AttrValue;
 import com.atguigu.lease.web.admin.service.AttrKeyService;
 import com.atguigu.lease.web.admin.service.AttrValueService;
 import com.atguigu.lease.web.admin.vo.attr.AttrKeyVo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -60,12 +61,26 @@ public class AttrController {
     @Operation(summary = "根据id删除属性名称")
     @DeleteMapping("key/deleteById")
     public Result removeAttrKeyById(@RequestParam Long attrKeyId) {
+
+        //TODO 根据id删除属性名称以及属性名称下面的所有值
+        //删除属性名称
+        attrKeyService.removeById(attrKeyId);
+
+        //删除属性值
+        LambdaQueryWrapper<AttrValue> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(AttrValue::getAttrKeyId,attrKeyId);
+        attrValueService.removeById(queryWrapper);
+
         return Result.ok();
     }
 
     @Operation(summary = "根据id删除属性值")
     @DeleteMapping("value/deleteById")
     public Result removeAttrValueById(@RequestParam Long id) {
+
+        //TODO 根据id删除属性值
+        attrValueService.removeById(id);
+
         return Result.ok();
     }
 
--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/FeeController.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/FeeController.java
@@ -4,9 +4,13 @@ package com.atguigu.lease.web.admin.controller.apartment;
 import com.atguigu.lease.common.result.Result;
 import com.atguigu.lease.model.entity.FeeKey;
 import com.atguigu.lease.model.entity.FeeValue;
+import com.atguigu.lease.web.admin.service.FeeKeyService;
+import com.atguigu.lease.web.admin.service.FeeValueService;
 import com.atguigu.lease.web.admin.vo.fee.FeeKeyVo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,15 +21,32 @@ import java.util.List;
 @RequestMapping("/admin/fee")
 public class FeeController {
 
+    @Autowired
+    private FeeKeyService feeKeyService;
+
+    @Autowired
+    private FeeValueService feeValueService;
+
+
     @Operation(summary = "保存或更新杂费名称")
     @PostMapping("key/saveOrUpdate")
     public Result saveOrUpdateFeeKey(@RequestBody FeeKey feeKey) {
+        //TODO 公寓杂费管理，保存或更新杂费名称
+
+        feeKeyService.saveOrUpdate(feeKey);
+
+
         return Result.ok();
     }
 
     @Operation(summary = "保存或更新杂费值")
     @PostMapping("value/saveOrUpdate")
     public Result saveOrUpdateFeeValue(@RequestBody FeeValue feeValue) {
+
+        //TODO 保存或更新杂费值
+
+        feeValueService.saveOrUpdate(feeValue);
+
         return Result.ok();
     }
 
@@ -33,18 +54,34 @@ public class FeeController {
     @Operation(summary = "查询全部杂费名称和杂费值列表")
     @GetMapping("list")
     public Result<List<FeeKeyVo>> feeInfoList() {
-        return Result.ok();
+        //TODO 查询全部杂费名称和杂费值
+
+        List<FeeKeyVo> list = feeKeyService.feeInfoList();
+
+        return Result.ok(list);
     }
 
     @Operation(summary = "根据id删除杂费名称")
     @DeleteMapping("key/deleteById")
     public Result deleteFeeKeyById(@RequestParam Long feeKeyId) {
+
+
+        //TODO 根据id删除杂费名称
+        //删除杂费名称
+        feeKeyService.removeById(feeKeyId);
+        //删除杂费名称下的杂费值
+        LambdaQueryWrapper<FeeValue> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(FeeValue::getFeeKeyId, feeKeyId);
+        feeValueService.remove(queryWrapper);
         return Result.ok();
     }
 
     @Operation(summary = "根据id删除杂费值")
     @DeleteMapping("value/deleteById")
     public Result deleteFeeValueById(@RequestParam Long id) {
+
+        //TODO 根据id删除杂费值
+        feeValueService.removeById(id);
         return Result.ok();
     }
 }
