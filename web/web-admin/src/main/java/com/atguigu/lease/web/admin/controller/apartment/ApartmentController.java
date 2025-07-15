f3d4ddd1476ee5fe68cbee3e09eb3ff4277127b9
yangxiaolan <1518634581@qq.com>
2025-07-15 10:02:31 +0800

    尚庭公寓第十三次提交，定位于121集 已完成：完成全部关于公寓信息的接口实现

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/ApartmentController.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/ApartmentController.java
@@ -9,6 +9,8 @@ import com.atguigu.lease.web.admin.vo.apartment.ApartmentDetailVo;
 import com.atguigu.lease.web.admin.vo.apartment.ApartmentItemVo;
 import com.atguigu.lease.web.admin.vo.apartment.ApartmentQueryVo;
 import com.atguigu.lease.web.admin.vo.apartment.ApartmentSubmitVo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.v3.oas.annotations.Operation;
@@ -43,9 +45,8 @@ public class ApartmentController {
     public Result<IPage<ApartmentItemVo>> pageItem(@RequestParam long current, @RequestParam long size, ApartmentQueryVo queryVo) {
         //TODO 根据条件分页查询公寓列表 分页查询！！
         //在配置类中配置一下分页工具
-        Page<ApartmentItemVo> page = new Page<>(current,size);
-        IPage<ApartmentItemVo> result =  service.pageItem(page,queryVo);
-
+        Page<ApartmentItemVo> page = new Page<>(current, size);
+        IPage<ApartmentItemVo> result = service.pageItem(page, queryVo);
 
 
         return Result.ok(result);
@@ -54,6 +55,7 @@ public class ApartmentController {
     @Operation(summary = "根据ID获取公寓详细信息")
     @GetMapping("getDetailById")
     public Result<ApartmentDetailVo> getDetailById(@RequestParam Long id) {
+        //TODO 根据id获取公寓详细信息
         ApartmentDetailVo result = service.getDetailById(id);
         return Result.ok(result);
     }
@@ -61,6 +63,7 @@ public class ApartmentController {
     @Operation(summary = "根据id删除公寓信息")
     @DeleteMapping("removeById")
     public Result removeById(@RequestParam Long id) {
+        //TODO 根据id删除公寓信息
         service.removeApartmentById(id);
         return Result.ok();
     }
@@ -68,13 +71,23 @@ public class ApartmentController {
     @Operation(summary = "根据id修改公寓发布状态")
     @PostMapping("updateReleaseStatusById")
     public Result updateReleaseStatusById(@RequestParam Long id, @RequestParam ReleaseStatus status) {
+        // TODO 根据id修改公寓发布状态
+        LambdaUpdateWrapper<ApartmentInfo> updateWrapper = new LambdaUpdateWrapper<>();
+        updateWrapper.eq(ApartmentInfo::getId, id);
+        updateWrapper.set(ApartmentInfo::getIsRelease, status);
+        service.update(updateWrapper);
+
         return Result.ok();
     }
 
     @Operation(summary = "根据区县id查询公寓信息列表")
     @GetMapping("listInfoByDistrictId")
     public Result<List<ApartmentInfo>> listInfoByDistrictId(@RequestParam Long id) {
-        return Result.ok();
+
+        LambdaQueryWrapper<ApartmentInfo> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ApartmentInfo::getDistrictId, id);
+        List<ApartmentInfo> list = service.list(queryWrapper);
+        return Result.ok(list);
     }
 }
 
