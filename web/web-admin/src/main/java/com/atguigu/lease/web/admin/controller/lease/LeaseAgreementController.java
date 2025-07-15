e6183b2e8b9eeddff0e61808f00a588fd2eeae0e
yangxiaolan <1518634581@qq.com>
2025-07-15 18:44:14 +0800

    尚庭公寓第十六次提交，定位于129集 已完成：完成租约管理相关的crud接口， 知识点：1.新增：在Controller层直接调用service层的saveOrUpdate()方法，参数传一个数据库表对应的类的实例        2.删除：在Controller层直接调用service层的removeById()方法，参数传一个所要删除表数据的id        3.修改：可以用LambdaUpdateWrapper的eq方法进行条件筛选，然后用set方法重新赋值        3.查询：根据返回类型确定是单表查询还是多表查询，单表查询只需要在controller层调用service层的get系列方法        如果涉及到多张表的查询需要自定义方法，注意结果集的映射关系

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/lease/LeaseAgreementController.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/lease/LeaseAgreementController.java
@@ -4,11 +4,15 @@ package com.atguigu.lease.web.admin.controller.lease;
 import com.atguigu.lease.common.result.Result;
 import com.atguigu.lease.model.entity.LeaseAgreement;
 import com.atguigu.lease.model.enums.LeaseStatus;
+import com.atguigu.lease.web.admin.service.LeaseAgreementService;
 import com.atguigu.lease.web.admin.vo.agreement.AgreementQueryVo;
 import com.atguigu.lease.web.admin.vo.agreement.AgreementVo;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -17,33 +21,54 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/admin/agreement")
 public class LeaseAgreementController {
 
+
+    @Autowired
+    private LeaseAgreementService service;
+
     @Operation(summary = "保存或修改租约信息")
     @PostMapping("saveOrUpdate")
     public Result saveOrUpdate(@RequestBody LeaseAgreement leaseAgreement) {
+        //TODO 保存或修改租约信息，简单单表更新接口
+        service.saveOrUpdate(leaseAgreement);
+
         return Result.ok();
     }
 
     @Operation(summary = "根据条件分页查询租约列表")
     @GetMapping("page")
     public Result<IPage<AgreementVo>> page(@RequestParam long current, @RequestParam long size, AgreementQueryVo queryVo) {
-        return Result.ok();
+        //TODO 根据条件分页查询租约列表
+        Page<AgreementVo> page = new Page<>();
+        IPage<AgreementVo> result =  service.pageAgreement(page,queryVo);
+
+
+        return Result.ok(result);
     }
 
     @Operation(summary = "根据id查询租约信息")
     @GetMapping(name = "getById")
     public Result<AgreementVo> getById(@RequestParam Long id) {
-        return Result.ok();
+        AgreementVo result =  service.getAgreementById(id);
+
+        return Result.ok(result);
     }
 
     @Operation(summary = "根据id删除租约信息")
     @DeleteMapping("removeById")
     public Result removeById(@RequestParam Long id) {
+
+        service.removeById(id);
         return Result.ok();
     }
 
     @Operation(summary = "根据id更新租约状态")
     @PostMapping("updateStatusById")
     public Result updateStatusById(@RequestParam Long id, @RequestParam LeaseStatus status) {
+        LambdaUpdateWrapper<LeaseAgreement> updateWrapper = new LambdaUpdateWrapper<>();
+        updateWrapper.eq(LeaseAgreement::getId,id);
+        updateWrapper.set(LeaseAgreement::getStatus,status);
+        service.update(updateWrapper);
+
         return Result.ok();
     }
 
--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/LeaseAgreementServiceImpl.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/LeaseAgreementServiceImpl.java
@@ -1,9 +1,15 @@
 package com.atguigu.lease.web.admin.service.impl;
 
-import com.atguigu.lease.model.entity.LeaseAgreement;
-import com.atguigu.lease.web.admin.mapper.LeaseAgreementMapper;
+import com.atguigu.lease.model.entity.*;
+import com.atguigu.lease.web.admin.mapper.*;
 import com.atguigu.lease.web.admin.service.LeaseAgreementService;
+import com.atguigu.lease.web.admin.vo.agreement.AgreementQueryVo;
+import com.atguigu.lease.web.admin.vo.agreement.AgreementVo;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,6 +21,56 @@ import org.springframework.stereotype.Service;
 public class LeaseAgreementServiceImpl extends ServiceImpl<LeaseAgreementMapper, LeaseAgreement>
         implements LeaseAgreementService {
 
+    @Autowired
+    private LeaseAgreementMapper leaseAgreementMapper;
+
+    @Autowired
+    private ApartmentInfoMapper apartmentInfoMapper;
+
+    @Autowired
+    private RoomInfoMapper roomInfoMapper;
+    @Autowired
+    private PaymentTypeMapper paymentTypeMapper;
+
+    @Autowired
+    private LeaseTermMapper leaseTermMapper;
+
+
+
+    @Override
+    public AgreementVo getAgreementById(Long id) {
+
+        //TODO 直接在service层完成数据库查询
+        //其实查询的本质就是将所需要的字段全部查出来然后打包在一个容器里返回给前端
+        //AgreementVo中的字段有四个自己的字段，且都是引用类型
+
+        LeaseAgreement leaseAgreement = leaseAgreementMapper.selectById(id);
+
+        ApartmentInfo apartmentInfo = apartmentInfoMapper.selectById(leaseAgreement.getApartmentId());
+
+        RoomInfo roomInfo = roomInfoMapper.selectById(leaseAgreement.getRoomId());
+
+        PaymentType paymentType = paymentTypeMapper.selectById(leaseAgreement.getPaymentTypeId());
+
+        LeaseTerm leaseTerm = leaseTermMapper.selectById(leaseAgreement.getLeaseTermId());
+
+
+        AgreementVo agreementVo = new AgreementVo();
+        BeanUtils.copyProperties(leaseAgreement,agreementVo);
+        agreementVo.setApartmentInfo(apartmentInfo);
+        agreementVo.setRoomInfo(roomInfo);
+        agreementVo.setPaymentType(paymentType);
+        agreementVo.setLeaseTerm(leaseTerm);
+
+        return  agreementVo;
+
+
+    }
+
+    @Override
+    public IPage<AgreementVo> pageAgreement(Page<AgreementVo> page, AgreementQueryVo queryVo) {
+        return leaseAgreementMapper.pageAgreement(page,queryVo);
+    }
 }
 
 
