a8c965dcb76406dde9e998bdb1c3ce5f4b0804a1
yangxiaolan <1518634581@qq.com>
2025-07-15 22:23:00 +0800

    尚庭公寓第十七次提交，定位于131集 已完成：完成租约管理的一个定时任务，单独一个ScheduleTasks类中实现，第一次接触定时任务 重点！：创建定时任务可以使用springframework提供的Schedule注解，作用在方法上，要注意的是注解参数中的cron表达式cron=“* * * * * * (*)”，从左到右一次表示:秒，分，时，日期，月份，星期，（年）， 这里使用定时任务的情景是：定时每天0点检查公寓租约是否到期，如果EndDate小于当前日期，那么查询那些租约状态为已签约和退租确认的数据，并执行修改租约状态为过期状态

--- web/web-admin/src/main/java/com/atguigu/lease/AdminWebApplication.java
+++ web/web-admin/src/main/java/com/atguigu/lease/AdminWebApplication.java
@@ -3,9 +3,10 @@ package com.atguigu.lease;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
-
+@EnableScheduling
 @MapperScan("com.atguigu.lease.web.*.mapper")
 @SpringBootApplication
 public class AdminWebApplication {
