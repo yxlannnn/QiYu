92d19a0df15d72cedd41bb2c555568309b7cf426
yxlannnn <1518634581@qq.com>
2025-07-23 21:24:06 +0800

    尚庭公寓第三十一次提交，定位于156集 已完成：完成app端找房下分页查询房间列表接口

--- web/web-app/src/main/java/com/atguigu/lease/web/app/service/impl/RoomInfoServiceImpl.java
+++ web/web-app/src/main/java/com/atguigu/lease/web/app/service/impl/RoomInfoServiceImpl.java
@@ -3,8 +3,13 @@ package com.atguigu.lease.web.app.service.impl;
 import com.atguigu.lease.model.entity.RoomInfo;
 import com.atguigu.lease.web.app.mapper.RoomInfoMapper;
 import com.atguigu.lease.web.app.service.RoomInfoService;
+import com.atguigu.lease.web.app.vo.room.RoomItemVo;
+import com.atguigu.lease.web.app.vo.room.RoomQueryVo;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,7 +21,14 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class RoomInfoServiceImpl extends ServiceImpl<RoomInfoMapper, RoomInfo>
         implements RoomInfoService {
+    @Autowired
+    private RoomInfoMapper roomInfoMapper;
 
+    @Override
+    public IPage<RoomItemVo> pageItem(Page<RoomItemVo> page, RoomQueryVo queryVo) {
+
+        return roomInfoMapper.pageItem(page,queryVo);
+    }
 }
 
 
